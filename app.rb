803d3f359bdd0d8304e03e26c6445c95f8e64ef0
Tarashish Mishra <sunu0000@gmail.com>
2014-05-26 23:34:04 +0530

    Indentation changes and added arduino_firmata stuff.

--- app.rb
+++ app.rb
@@ -3,35 +3,35 @@ require 'em-websocket'
 require 'sinatra/base'
 
 EM.run do
-  class App < Sinatra::Base
-    get '/' do
-      erb :index
-    end
-    set :bind, '0.0.0.0'
-  end
-
-  @clients = []
-
-  EM::WebSocket.start(:host => '0.0.0.0', :port => '3001') do |ws|
-    ws.onopen do |handshake|
-      @clients << ws
-      ws.send "Connected to #{handshake.path}."
-      puts "Connected"
+    class App < Sinatra::Base
+        get '/' do
+            erb :index
+        end
+        set :bind, '0.0.0.0'
     end
 
-    ws.onclose do
-      ws.send "Closed."
-      puts "closed"
-      @clients.delete ws
+    @clients = []
+
+    EM::WebSocket.start(:host => '0.0.0.0', :port => '3001') do |ws|
+        ws.onopen do |handshake|
+            @clients << ws
+            ws.send "Connected to #{handshake.path}."
+            puts "Connected"
+        end
+
+        ws.onclose do
+            ws.send "Closed."
+            puts "closed"
+            @clients.delete ws
+        end
+
+        ws.onmessage do |msg|
+            puts "Received Message: #{msg}"
+            @clients.each do |socket|
+                socket.send msg
+            end
+        end
     end
 
-    ws.onmessage do |msg|
-      puts "Received Message: #{msg}"
-      @clients.each do |socket|
-        socket.send msg
-      end
-    end
-  end
-
-  App.run! :port => 3000
+    App.run! :port => 3000
 end
--- client.rb
+++ client.rb
@@ -1,22 +1,30 @@
 require_relative 'em-websocket-client'
+require 'arduino_firmata'
+
+arduino = ArduinoFirmata.connect
 
 EM.run do
-  conn = EventMachine::WebSocketClient.connect("ws://localhost:3001/")
+    conn = EventMachine::WebSocketClient.connect("ws://0.0.0.0:3001/")
 
-  conn.callback do
-    conn.send_msg "connected!"
-  end
+    conn.callback do
+        conn.send_msg "connected!"
+    end
 
-  conn.errback do |e|
-    puts "Got error: #{e}"
-  end
+    conn.errback do |e|
+        puts "Got error: #{e}"
+    end
 
-  conn.stream do |msg|
-    puts "<#{msg}>"
-  end
+    conn.stream do |msg|
+        puts msg
+        if String(msg) == "f"
+            arduino.digital_write 13, true
+        elsif String(msg) == 'b'
+            arduino.digital_write 13, false
+        end
+    end
 
-  conn.disconnect do
-    puts "disconnected!"
-    EM::stop_event_loop
-  end
+    conn.disconnect do
+        puts "disconnected!"
+        EM::stop_event_loop
+    end
 end
